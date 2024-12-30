937d0238da7272aa865cb9f693c7be5b516cb3a4
hosein ladmakhi <ladmakhi81@gmail.com>
2024-12-30 12:40:20 +0330

    feat: create migration for token

---
+++ migrations/20241230090444_create_token_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "_tokens" (
+    "id"            BIGSERIAL      PRIMARY KEY,
+    "created_at"    TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
+    "updated_at"    TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
+    "access_token"  TEXT           NOT NULL,
+    "refresh_token" TEXT           NOT NULL,
+    "user_id"       BIGINT         NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY ("user_id") REFERENCES "_users"("id") ON DELETE NO ACTION
+)
